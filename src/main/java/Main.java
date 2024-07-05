709e61b7f025ab6451f0f86f3117a153bc659f66
Pourya <pourya.nejadensan@gmail.com>
2024-07-05 15:10:01 +0200

    Step 6: In the 'main' method, create an object of the 'MediaController' class and use it to play both a song and a video.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Playable mp = new MusicPlayer();
         Playable vp = new VideoPlayer();
+
+        MediaController mc = new MediaController();
+        mc.playMedia(mp);
+        mc.playMedia(vp);
     }
 }
